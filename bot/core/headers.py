45e925d9ebb49b220f8353bae978dfde0205717b
KHONDOKER X HASAN <80763315+hasan1818666891@users.noreply.github.com>
2024-10-29 16:31:14 +0600

    Add files via upload

---
+++ bot/core/headers.py
@@ -0,0 +1,17 @@
+headers = {
+    'User-Agent': "Mozilla/5.0 (Linux; Android 14) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.6422.165 Mobile Safari/537.36",
+  'Accept': "application/json",
+  'Accept-Encoding': "gzip, deflate, br, zstd",
+  'Content-Type': "application/json",
+  'sec-ch-ua-platform': "\"Android\"",
+  'sec-ch-ua': "\"Android WebView\";v=\"129\", \"Not=A?Brand\";v=\"8\", \"Chromium\";v=\"129\"",
+  'sec-ch-ua-mobile': "?1",
+  'origin': "https://app.paws.community",
+  'x-requested-with': "org.telegram.messenger",
+  'sec-fetch-site': "same-site",
+  'sec-fetch-mode': "cors",
+  'sec-fetch-dest': "empty",
+  'referer': "https://app.paws.community/",
+  'accept-language': "en,en-US;q=0.9,bn-BD;q=0.8,bn;q=0.7",
+  'priority': "u=1, i"
+}
