65be3a4891082a119dbd8e5b162381dd67dd384e
KHONDOKER X HASAN <80763315+hasan1818666891@users.noreply.github.com>
2024-10-29 16:29:44 +0600

    Add files via upload

---
+++ bot/config/config.py
@@ -0,0 +1,18 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", env_ignore_empty=True)
+
+    API_ID: int
+    API_HASH: str
+
+    SLEEP_TIME: list[int] = [41200, 43200] #wait 12 hours
+    START_DELAY: list[int] = [5, 25]
+    AUTO_TASK: bool = True
+    JOIN_TG_CHANNELS: bool = False
+    REF_ID: str = 'acAO24ki'
+    DISABLED_TASKS: list[str] = ['wallet', 'invite']
+
+
+settings = Settings()
