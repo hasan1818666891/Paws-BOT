5a8f85ee530771256ef88e72a1fc46e2d78ed3e0
KHONDOKER X HASAN <80763315+hasan1818666891@users.noreply.github.com>
2024-11-04 07:44:19 +0600

    🛠️ Fixed Wallet blind problem

--- bot/config/config.py
+++ bot/config/config.py
@@ -9,10 +9,13 @@ class Settings(BaseSettings):
 
     SLEEP_TIME: list[int] = [41200, 43200] #wait 12 hours
     START_DELAY: list[int] = [5, 25]
+    
     AUTO_TASK: bool = True
     JOIN_TG_CHANNELS: bool = False
+    AUTO_ADD_WALLET: bool = False
+    
     REF_ID: str = 'acAO24ki'
-    DISABLED_TASKS: list[str] = ['wallet', 'invite']
+    DISABLED_TASKS: list[str] = ['invite', 'boost']
 
 
 settings = Settings()
